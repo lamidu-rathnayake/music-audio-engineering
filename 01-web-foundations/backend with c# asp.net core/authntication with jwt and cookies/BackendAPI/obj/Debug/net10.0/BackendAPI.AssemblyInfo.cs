be8631825e45dbbdedd2cebd2e38ee39f8c4c586
lamidu rathnayake <lamidurathnayakeofficial@gmail.com>
2026-09-11 16:51:36 +0530

    added the .gitignore

--- 01-web-foundations/backend with c# asp.net core/authntication with jwt and cookies/BackendAPI/obj/Debug/net10.0/BackendAPI.AssemblyInfo.cs
+++ 01-web-foundations/backend with c# asp.net core/authntication with jwt and cookies/BackendAPI/obj/Debug/net10.0/BackendAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52a57aae5c1df09b5fcb0ecb22a463edced097f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa36237f19e062c060cc7a615cbae4912993eef5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
